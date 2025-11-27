3cec2d050ba84f3c23c7df114f763d534700435f
Amaru Tom <tom.amaru09@gmail.com>
2025-11-27 20:51:33 +0100

    feat(page): add About component with main tag for sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import Header from "../components/layout/Header";
 import Hero from "../components/home/Hero";
+import About from "../components/home/About";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Hero />
+      <main className="w-full h-full flex flex-col items-center justify-center gap-50 lg:gap-80">
+        <Hero />
+        <About />
+      </main>
     </>
   );
 }
