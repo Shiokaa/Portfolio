1b21dc42c7a42a5d49107dcb551267f7a55f5b88
Shioka <tom.amaru09@gmail.com>
2025-11-28 02:40:02 +0100

    feat(data): add projects data

---
+++ data/projects.ts
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    id: 1,
+    name: "Groupie Tracker",
+    description: "Groupie Tracker est une application web utilisant l'API Rick et Morty pour afficher les personnages, leurs épisodes et leurs locations. C'est un projet scolaire réalisé en solo en première année de Bachelor Informatique à Ynov Aix Campus.",
+    images: ["", "", ""],
+    technologies: ["", "", ""],
+    github: "",
+    thumbnail: "",
+    year: "2025",
+  },
+];
