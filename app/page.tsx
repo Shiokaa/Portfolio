d18fd4663479a23594dc08e6069ba93122e4dbed
Shioka <tom.amaru09@gmail.com>
2025-11-28 21:17:09 +0100

    style(page): change gap values on main tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@
 import Header from "../components/layout/Header";
 import Hero from "../components/home/Hero";
 import About from "../components/home/About";
+import Projects from "../components/home/Projects";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="w-full h-full flex flex-col items-center justify-center gap-50 lg:gap-80">
+      <main className="w-full h-full flex flex-col items-center justify-center gap-36 lg:gap-66">
         <Hero />
         <About />
+        <Projects />
       </main>
     </>
   );
