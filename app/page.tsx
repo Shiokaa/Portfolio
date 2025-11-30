50e98ee9d667911db7dfdc508bc30280a601b8cc
Shioka <tom.amaru09@gmail.com>
2025-11-30 01:11:39 +0100

    feat(page): add Animation on every section

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,25 @@ import About from "@/components/home/About";
 import Projects from "@/components/home/Projects";
 import Contact from "@/components/home/Contact";
 import Footer from "@/components/layout/Footer";
+import RevealOnScroll from "@/components/ui/RevealOnScroll";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className="flex flex-col items-center justify-center gap-36 lg:gap-66 py-24 lg:py-65 flex-1">
-        <Hero />
-        <About />
-        <Projects />
-        <Contact />
+        <RevealOnScroll>
+          <Hero />
+        </RevealOnScroll>
+        <RevealOnScroll>
+          <About />
+        </RevealOnScroll>
+        <RevealOnScroll>
+          <Projects />
+        </RevealOnScroll>
+        <RevealOnScroll>
+          <Contact />
+        </RevealOnScroll>
       </main>
       <Footer />
     </>
